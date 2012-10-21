b526f8d2c8ba1c0be467ddef6eb0d0c47658bd8e
Henric Edwards <henric.edwards@telia.com>
2012-10-21 20:58:08 +0200

    Added missing Command-files

---
+++ src/WeenyBoard/WeenyBoard/Commands/Commands.cs
@@ -0,0 +1,16 @@
+﻿namespace WeenyBoard.Commands
+{
+    public class Commands
+    {
+    }
+
+    public class AddBoardItemCommand : ICommand
+    {
+        public void AddBoardItem()
+        {
+            
+        }
+    }
+
+    public interface ICommand{}
+}
